fa101400dd6ec794a99e82de4ac112bfec10afd7
NikkiW96 <43293583+NikkiW96@users.noreply.github.com>
2018-10-12 19:45:52 +0530

    python file created

---
+++ printing.py
@@ -0,0 +1,8 @@
+#First Python Code In Github
+print("HEY GUYS!")
+print("Python is used for: web development (server-side), software development, mathematics, system scripting,etc.")
+x=5
+print(x)
+
+
+#THANK YOU:)
