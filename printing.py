f6fe5f91617ca426ce3985f37b801d0c2d179dcb
NikkiW96 <43293583+NikkiW96@users.noreply.github.com>
2018-10-12 19:53:46 +0530

    adding

--- printing.py
+++ printing.py
@@ -3,6 +3,10 @@ print("HEY GUYS!")
 print("Python is used for: web development (server-side), software development, mathematics, system scripting,etc.")
 x=5
 print(x)
+#arithmetic operations
+a=10
+b=90
+print(a+b)
 
 
 #THANK YOU:)
